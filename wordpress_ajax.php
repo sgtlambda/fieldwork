0c54a9382088f1a5e7e9167ab2c052baddaf059a
JM Versteeg <janmerijnversteeg@gmail.com>
2014-10-02 09:19:18 +0200

    fix blog header require

--- wordpress_ajax.php
+++ wordpress_ajax.php
@@ -1,7 +1,7 @@
 <?php
 
 define('WP_USE_THEMES', false);
-require('../../wp-blog-header.php');
+require('../../../wp-blog-header.php');
 header("HTTP/1.1 200 OK");
 
 $data = new JannieFormResults($_POST);
