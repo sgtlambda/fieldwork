d9db87db55d1f1ee7981d3a4227e86a63ee1ae8d
JM Versteeg <janmerijnversteeg@gmail.com>
2014-10-03 08:34:12 +0200

    fixed jslint warns

--- js/jannietooltips.js
+++ js/jannietooltips.js
@@ -1,5 +1,5 @@
 var JannieTooltips;
-(function($){
+(function($, window, document){
     JannieTooltips = {
         active: -1,
         activeTooltip: -1,
@@ -12,8 +12,6 @@ var JannieTooltips;
             else{
                 var elmtOffset = elmt.offset();
                 this.activeTooltip.css({
-                    //top: elmtOffset.top + elmt.outerHeight() / 2 - this.activeTooltip.outerHeight() / 2,
-                    //left: elmtOffset.left - this.activeTooltip.outerWidth()
                     top: elmtOffset.top + elmt.outerHeight(),
                     left: elmtOffset.left + elmt.outerWidth()/2 - this.activeTooltip.outerWidth()/2
                 });
@@ -28,7 +26,7 @@ var JannieTooltips;
             });
         },
         dismiss: function(){
-            if(this.trackInterval!=-1){
+            if(this.trackInterval!==-1){
                 window.clearInterval(this.trackInterval);
                 this.trackInterval = -1;
             }
@@ -45,14 +43,13 @@ var JannieTooltips;
             this.active = this.activeTooltip = -1;
         },
         isActive: function(elmt){
-            if(this.active!=-1)
+            if(this.active!==-1)
                 return this.active[0] === elmt[0];
             return false;
         }
     };
     $.fn.jtLink = function(html, showOn, hideOn){
         var elmt = this;
-        // console.log(elmt);
         elmt.data('jt', {
             html: html
         });
@@ -63,7 +60,7 @@ var JannieTooltips;
         for(var n in hideOn)
             elmt.on(showOn[n]+".jt", function(){
                 $(this).jtHide();
-            })
+            });
     };
     $.fn.jtUnlink = function(){
         var elmt = this;
@@ -71,11 +68,11 @@ var JannieTooltips;
         elmt.removeData('jt');
         elmt.removeData('jt-tt');
         elmt.off('.jt');
-    }
+    };
     $.fn.jtShow = function(){
         var elmt = this;
         if(!JannieTooltips.isActive(elmt)){
-            if(JannieTooltips.active != -1)
+            if(JannieTooltips.active !== -1)
                 JannieTooltips.dismiss();
             JannieTooltips.active = elmt;
             var newTooltip = $('<div class="jt-wrapper"><div class="jt-arrow"></div><div class="jt-inner">' + elmt.data('jt').html + '</div></div>').css({
@@ -90,16 +87,17 @@ var JannieTooltips;
             newTooltip.css({
                 top: elmtOffset.top + elmt.outerHeight(),
                 left: elmtOffset.left + elmt.outerWidth()/2 - newTooltip.outerWidth()/2
+                // todo this is redundant code (see track function above)
             });
             JannieTooltips.activeTooltip = newTooltip;
             JannieTooltips.startTrack();
         }
         return this;
-    }
+    };
     $.fn.jtHide = function(){
         var elmt = this;
         if(JannieTooltips.isActive(elmt))
             JannieTooltips.dismiss();
         return this;
-    }
-})(jQuery);
+    };
+})(jQuery, window, document);
