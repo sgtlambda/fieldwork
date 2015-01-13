22a35917598bebbda370277000e41a30d812e921
JM Versteeg <janmerijnversteeg@gmail.com>
2015-01-13 15:17:26 +0100

    script enqueues

--- wordpress.php
+++ wordpress.php
@@ -9,12 +9,12 @@
 require __DIR__ . '/autoload.php';
 
 add_action('wp_enqueue_scripts', function () {
-    $vendorPath = plugins_url('bower_components/', __FILE__);
+    $bower_components = plugins_url('bower_components/', __FILE__);
 
-    wp_register_script('underscore', $vendorPath . 'underscore/underscore-min.js', '1.7.0');
-    wp_register_script('jquery-maskedinputs', $vendorPath . 'jquery-maskedinputs/dist/jquery.maskedinput.min.js', array('jquery'), '1.4.0');
-    wp_register_script('datetimepicker', $vendorPath . 'datetimepicker/jquery.datetimepicker.js', array('jquery'), '2.4.1');
-    wp_register_script('sweetalert', $vendorPath . 'sweetalert/lib/sweet-alert.min.js', array(), '0.3.3');
+    wp_register_script('jquery-maskedinputs', $bower_components . 'jquery-maskedinputs/dist/jquery.maskedinput.min.js', array('jquery'), '1.4.0');
+    wp_register_script('underscore', $bower_components . 'underscore/underscore-min.js', '1.7.0');
+    wp_register_script('datetimepicker', $bower_components . 'datetimepicker/jquery.datetimepicker.js', array('jquery'), '2.4.1');
+    wp_register_script('sweetalert', $bower_components . 'sweetalert/lib/sweet-alert.min.js', array(), '0.3.3');
 
     $deps = array(
         'jquery',
@@ -24,9 +24,7 @@ add_action('wp_enqueue_scripts', function () {
         'sweetalert'
     );
 
-    wp_register_script('jannieforms', plugins_url('dist/jannieforms.min.js', __FILE__), $deps);
-
-    wp_enqueue_script('jannieforms');
+    wp_enqueue_script('jannieforms', plugins_url('dist/jannieforms.min.js', __FILE__), $deps);
     wp_enqueue_style('jannieforms', plugins_url('dist/jannieforms.css', __FILE__));
 });
 
