756d0cb556dd15a890d625e8b9892c36e4d9bad1
jmversteeg <janmerijnversteeg@gmail.com>
2015-02-12 14:37:38 +0100

    Use iban validation library

--- src/fieldwork/validators/IbanFieldValidator.php
+++ src/fieldwork/validators/IbanFieldValidator.php
@@ -2,6 +2,8 @@
 
 namespace fieldwork\validators;
 
+use IBAN\Validation\IBANValidator;
+
 class IbanFieldValidator extends RegexFieldValidator
 {
 
@@ -9,11 +11,19 @@ class IbanFieldValidator extends RegexFieldValidator
     const PATT_LOCAL = "/^([A-Z]{2}[0-9]{2} [A-Z0-9]{4} [0-9]{4} [0-9]{4}( [0-9]{4})?( [0-9]{2})?|[0-9]{1,10})$/";
     const ERROR      = "Ongeldig rekeningnummer";
 
+    private $validator;
+
     public function __construct ()
     {
         parent::__construct(
             self::PATT, self::ERROR, self::PATT_LOCAL
         );
+        $this->validator = new IBANValidator();
+    }
+
+    public function isValid ($value)
+    {
+        return $this->validator->validate($value);
     }
 
 }
