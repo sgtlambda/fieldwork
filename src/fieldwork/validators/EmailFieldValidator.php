2c802b00fe5202a781984bc146c013f88ecb84f4
jmversteeg <janmerijnversteeg@gmail.com>
2015-02-12 14:10:43 +0100

    Use external library for more accurate email validation

--- src/fieldwork/validators/EmailFieldValidator.php
+++ src/fieldwork/validators/EmailFieldValidator.php
@@ -2,17 +2,25 @@
 
 namespace fieldwork\validators;
 
-
+use Egulias\EmailValidator\EmailValidator;
 
 class EmailFieldValidator extends RegexFieldValidator
 {
 
-    const PATT  = "/^[A-Z0-9._%\\-+]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$/i";
-    const ERROR = "Vul een geldig e-mailadres in.";
+    const PATT  = "/^.*@.*\\.[A-Z]{2,}$/i"; // This is a very simple version of the email validation regular expression which is only used client-side.
+    const ERROR = "Enter a valid email address";
+
+    private $validator;
 
     public function __construct ()
     {
         parent::__construct(self::PATT, self::ERROR);
+        $this->validator = new EmailValidator();
+    }
+
+    public function isValid ($value)
+    {
+        return $this->validator->isValid($value);
     }
 
 }
