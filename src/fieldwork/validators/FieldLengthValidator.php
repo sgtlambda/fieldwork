fa80c341a28cec886141dc354d2ca8a302a92d67
jmversteeg <janmerijnversteeg@gmail.com>
2015-03-09 11:36:00 +0100

    Add $fieldRequired param to FieldLengthValidator constructor

--- src/fieldwork/validators/FieldLengthValidator.php
+++ src/fieldwork/validators/FieldLengthValidator.php
@@ -5,9 +5,16 @@ namespace fieldwork\validators;
 class FieldLengthValidator extends RegexFieldValidator
 {
 
-    public function __construct ($min, $max, $allowed = ".", $error = "Input must consist of at least %d and no more than %d characters")
+    /**
+     * @param int    $min          The minimum number of allowed characters
+     * @param int    $max          The maximum number of allowed characters
+     * @param string $allowed      The allowed characters as a regex sequence
+     * @param string $error        The string to display if the field does not pass validation
+     * @param bool   $requireInput If false, the user can also leave the field empty
+     */
+    public function __construct ($min, $max, $allowed = ".", $error = "Input must consist of at least %d and no more than %d characters", $requireInput = true)
     {
-        parent::__construct("/^" . $allowed . "{" . $min . "," . $max . "}$/", sprintf($error, $min, $max));
+        parent::__construct("/^(" . $allowed . "{" . $min . "," . $max . "})" . ($requireInput ? "" : "?") . "$/", sprintf($error, $min, $max));
     }
 
 }
