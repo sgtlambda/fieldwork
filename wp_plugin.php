8f4e0f37fa1141d75d52068640b9b47baec37a39
jmversteeg <janmerijnversteeg@gmail.com>
2015-03-25 12:01:08 +0100

    Enqueue assets in admin area as well

--- wp_plugin.php
+++ wp_plugin.php
@@ -25,7 +25,8 @@ $loader = new \Aura\Autoload\Loader();
 $loader->addPrefix('fieldwork', __DIR__ . '/src/fieldwork');
 $loader->register();
 
-add_action('wp_enqueue_scripts', function () {
+function fieldworkEnqueueScripts ()
+{
     $bower_components = plugins_url('bower_components/', __FILE__);
 
     wp_register_script('jquery-maskedinputs', $bower_components . 'jquery-maskedinputs/dist/jquery.maskedinput.min.js', array('jquery'), '1.4.0');
@@ -43,7 +44,10 @@ add_action('wp_enqueue_scripts', function () {
 
     wp_enqueue_script('fieldwork', plugins_url('dist/fieldwork.min.js', __FILE__), $deps);
     wp_enqueue_style('fieldwork', plugins_url('dist/fieldwork.css', __FILE__));
-});
+}
+
+add_action('wp_enqueue_scripts', 'fieldworkEnqueueScripts');
+add_action('admin_enqueue_scripts', 'fieldworkEnqueueScripts');
 
 add_action('plugins_loaded', function () {
 
