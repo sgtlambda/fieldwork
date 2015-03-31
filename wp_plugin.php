a692562930e5c154f84a12e05e17c25e1e16b077
jmversteeg <janmerijnversteeg@gmail.com>
2015-03-31 14:40:48 +0200

    Remove some wrong enqueues

--- wp_plugin.php
+++ wp_plugin.php
@@ -27,19 +27,8 @@ $loader->register();
 
 function fieldworkEnqueueScripts ()
 {
-    $bower_components = plugins_url('bower_components/', __FILE__);
-
-    wp_register_script('jquery-maskedinputs', $bower_components . 'jquery-maskedinputs/dist/jquery.maskedinput.min.js', array('jquery'), '1.4.0');
-    wp_register_script('underscore', $bower_components . 'underscore/underscore-min.js', '1.7.0');
-    wp_register_script('datetimepicker', $bower_components . 'datetimepicker/jquery.datetimepicker.js', array('jquery'), '2.4.1');
-    wp_register_script('sweetalert', $bower_components . 'sweetalert/lib/sweet-alert.min.js', array(), '0.3.3');
-
     $deps = array(
         'jquery',
-        'jquery-maskedinputs',
-        'underscore',
-        'datetimepicker',
-        'sweetalert'
     );
 
     wp_enqueue_script('fieldwork', plugins_url('dist/fieldwork.min.js', __FILE__), $deps);
