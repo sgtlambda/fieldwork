a38d9c1f88efbe8a2f7d27336c56ec74f096dbba
JM Versteeg <janmerijnversteeg@gmail.com>
2016-03-08 12:16:47 +0100

    Use other iban library

--- src/fieldwork/validators/IbanFieldValidator.php
+++ src/fieldwork/validators/IbanFieldValidator.php
@@ -11,19 +11,16 @@ class IbanFieldValidator extends RegexFieldValidator
     const PATT_BBAN = "/^([A-Z]{2}[0-9]{2} [A-Z0-9]{4} [0-9]{4} [0-9]{4}( [0-9]{4})?( [0-9]{2})?|[0-9]{1,10})$/";
     const ERROR     = "Not a valid IBAN";
 
-    private $validator;
-
     public function __construct ($convertBban = false, $errorMsg = self::ERROR)
     {
         parent::__construct(
             self::PATT, $errorMsg, $convertBban ? self::PATT_BBAN : self::PATT
         );
-        $this->validator = new IBANValidator();
     }
 
     public function isValid ($value)
     {
         $sanitized = preg_replace('/\s/', '', $value);
-        return $this->validator->validate($sanitized);
+        return verify_iban($sanitized, true);
     }
 }
